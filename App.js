53d196c3fd2fdbfa96e2b58702ffd0d8d1d81c1e
polawat phetra <pphetra@gmail.com>
2018-10-07 17:38:02 +0700

    add onboarding screen

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation'
+import { createBottomTabNavigator, createSwitchNavigator } from 'react-navigation'
 
 import ReportMap from './app/screens/ReportMap';
-import ReportList from './app/screens/ReportList'
+import ReportList from './app/screens/ReportList';
+import OnBoarding from './app/screens/Onboarding';
 
-
-const App = createBottomTabNavigator({
+const Main = createBottomTabNavigator({
   Map: ReportMap,
   List: ReportList
 });
+
+// using switchNavigator so user can't click back button from mainNavigator
+const App = createSwitchNavigator({
+  OnBoarding: OnBoarding,
+  Main: Main,
+})
 
 export default App;
---
+++ app/screens/Onboarding.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+const slides = [
+    {
+      key: 'somethun',
+      title: 'Title 1',
+      text: 'Description.\nSay something cool',      
+      backgroundColor: '#59b2ab',
+    },
+    {
+      key: 'somethun-dos',
+      title: 'Title 2',
+      text: 'Other cool stuff',
+      backgroundColor: '#febe29',
+    },
+    {
+      key: 'somethun1',
+      title: 'Rocket guy',
+      text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+      backgroundColor: '#22bcb5',
+    }
+  ];
+
+export default class OnBoardingScreen extends React.Component {
+
+
+
+    _onDone = () => {
+        // User finished the introduction. Show real app through
+        // navigation or simply by controlling state
+        this.props.navigation.navigate('Main')
+    }
+
+    render() {
+        return (
+            <AppIntroSlider slides={slides} onDone={this._onDone}/>
+        )
+    }
+}
